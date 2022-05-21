8e800c22611b10a243fd086637acbc0bbc858e25
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-05-20 21:39:37 -0600

    Crea función para buscar canciones

---
+++ js/app.js
@@ -0,0 +1,25 @@
+import * as UI from "./interfaz.js";
+
+UI.formularioBuscar.addEventListener("submit", buscarCancion);
+
+function buscarCancion(e) {
+  e.preventDefault();
+
+  // Obtener datos del formulario
+  const artista = document.querySelector("#artista").value;
+  const cancion = document.querySelector("#cancion").value;
+
+  if (artista === "" || cancion === "") {
+    // El usuario dejo un campo vacio, mostrar error
+
+    UI.divMensajes.textContent = "Error... Todos los campos son abligatorios";
+    UI.divMensajes.classList.add("error");
+
+    setTimeout(() => {
+      UI.divMensajes.textContent = "";
+      UI.divMensajes.classList.remove("error");
+    }, 3000);
+  }
+
+  // Consultar la API
+}
